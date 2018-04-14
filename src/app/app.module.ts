2d8f16de20d71c99ee9bdb61978b4318647879b9
imolinuevo <i.m.parron@opendeusto.es>
2018-04-14 18:31:43 +0200

    Add devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { postReducer } from './reducers/post.reducer';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -14,6 +15,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     StoreModule.forRoot({
       post: postReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10 // number of states to retain
     }),
     FormsModule
   ],
